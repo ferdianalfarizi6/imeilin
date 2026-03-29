37ac914919585cc3a3f28f76fd08d1ae9cd08bb6
ferdianalfarizi6 <ferdian_alfarizi@teknokrat.ac.id>
2026-03-30 03:06:12 +0700

    Force clean APP_URL for Vite assets on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,12 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production' || str_contains(config('app.url'), 'https://')) {
             URL::forceScheme('https');
         }
+
+        // Paksa URL Root agar terhindar dari /public/ jika environment tidak sinkron
+        if (config('app.env') === 'production') {
+            config(['app.asset_url' => config('app.url')]);
+            URL::forceRootUrl(config('app.url'));
+            app('url')->formatRoot(config('app.url'));
+        }
     }
 }
